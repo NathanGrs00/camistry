66f4f0b72d3faa4957ff1354aaf9a8c8c9dccc02
NathanGRSDesktop <nathangeers00@gmail.com>
2025-05-18 20:45:44 +0200

    Add HamburgerFragment and implement menu toggle functionality in TopbarFragment

--- app/src/main/java/com/nathan/camistry/ui/topbar/TopbarFragment.kt
+++ app/src/main/java/com/nathan/camistry/ui/topbar/TopbarFragment.kt
@@ -1,10 +1,13 @@
 package com.nathan.camistry.ui.topbar
 
+import android.animation.ObjectAnimator
+import android.view.MotionEvent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
 import com.nathan.camistry.R
 
@@ -27,8 +30,34 @@ class TopbarFragment : Fragment() {
             //TODO: Send to profile page
         }
 
+        // If the user clicks on the menu icon, show the hamburger menu and the blocker
         ivMenu.setOnClickListener {
-            // TODO: Open side menu
+            val menu = requireActivity().findViewById<View>(R.id.fcv_hamburger)
+            val blocker = requireActivity().findViewById<View>(R.id.overlay_blocker)
+            menu.visibility = View.VISIBLE
+            blocker.visibility = View.VISIBLE
+
+            menu.post {
+                ObjectAnimator.ofFloat(menu, "translationX", -menu.width.toFloat(), 0f).apply {
+                    duration = 400
+                    start()
+                }
+            }
+
+            // If the user clicks on the blocker, close the menu
+            blocker.setOnTouchListener { _, event ->
+                if (event.action == MotionEvent.ACTION_UP) {
+                    blocker.performClick()
+                    ObjectAnimator.ofFloat(menu, "translationX", 0f, -menu.width.toFloat()).apply {
+                        duration = 400
+                        start()
+                    }.doOnEnd {
+                        menu.visibility = View.GONE
+                        blocker.visibility = View.GONE
+                    }
+                }
+                true
+            }
         }
     }
 }
