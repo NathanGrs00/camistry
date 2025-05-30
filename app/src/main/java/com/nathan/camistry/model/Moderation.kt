7e8bf926e1fab0358091dc041f5384b2e1d4eed9
NathanGRSDesktop <nathangeers00@gmail.com>
2025-05-30 23:52:44 +0200

    Add Connection, DiscoverPreferences, and Moderation models; update build dependencies

---
+++ app/src/main/java/com/nathan/camistry/model/Moderation.kt
@@ -0,0 +1,16 @@
+package com.nathan.camistry.model
+
+import com.google.firebase.Timestamp
+
+data class Report(
+    val reportedUserId: String,
+    val reporterUserId: String,
+    val reason: String,
+    val timestamp: Timestamp
+)
+
+data class Block(
+    val blockedByUserId: String,
+    val blockedUserId: String,
+    val timestamp: Timestamp
+)
