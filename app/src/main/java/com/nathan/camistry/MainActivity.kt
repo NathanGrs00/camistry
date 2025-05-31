c1762e67a9b9c717adce7abf975c1c2d39646197
NathanGRSDesktop <nathangeers00@gmail.com>
2025-05-31 23:26:30 +0200

    Enhance UI layout and integrate Glide for image loading; update dependencies

--- app/src/main/java/com/nathan/camistry/MainActivity.kt
+++ app/src/main/java/com/nathan/camistry/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.nathan.camistry
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.bumptech.glide.Glide
+import com.nathan.camistry.model.PublicProfile
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val topbarHeight = resources.getDimensionPixelSize(R.dimen.top_bar_height)
         // Find the linear layout, where all the blocks will go in.
         val rootLayout = findViewById<LinearLayout>(R.id.ll_main_content)
+        rootLayout.setPadding(
+            rootLayout.paddingLeft,
+            topbarHeight,
+            rootLayout.paddingRight,
+            rootLayout.paddingBottom
+        )
+
         // Make an inflater to show the blocks
         val inflater = LayoutInflater.from(this)
 
@@ -20,10 +34,40 @@ class MainActivity : AppCompatActivity() {
         val leadPhotoView = inflater.inflate(R.layout.content_lead, rootLayout, false)
         val blockBioView = inflater.inflate(R.layout.content_bio, rootLayout, false)
 
-        //TODO: Make the bio dynamic.
-        leadPhotoView.findViewById<TextView>(R.id.tv_firstname).text = "Bob"
-        leadPhotoView.findViewById<TextView>(R.id.tv_age).text = "25"
-        blockBioView.findViewById<TextView>(R.id.tv_bio).text = "Hello, my name is Bob."
+        val dummyUser = PublicProfile(
+            id = "12345",
+            firstName = "Bob",
+            age = 25,
+            bio = "Hello, my name is Bob.",
+            heightCm = 184,
+            gender = "Male",
+            orientation = "Straight",
+            intentions = "Long-term relationship",
+            languages = listOf("English", "Spanish"),
+            location = Location("New York"),
+            jobTitle = "Mechanical Engineer",
+            education = "Bachelor's in Mechanical Engineering",
+            interests = listOf("Hiking", "Cooking", "Reading"),
+            smokes = false,
+            drinks = true,
+            hasPets = "No, but I love animals",
+            wantsChildren = "I don't know yet",
+            photos = listOf("android.resource://com.nathan.camistry/drawable/dummyphoto"),
+            isVerified = true,
+            isOnline = true,
+        )
+
+        dummyUser.photos[0].let { photoUrl ->
+            Glide.with(this)
+                .load(photoUrl)
+                // TODO: Make placeholder image (user icon?)
+                .into(leadPhotoView.findViewById(R.id.iv_picture_lead)
+            )
+        }
+
+        leadPhotoView.findViewById<TextView>(R.id.tv_firstname).text = dummyUser.firstName
+        leadPhotoView.findViewById<TextView>(R.id.tv_age).text = dummyUser.age.toString()
+        blockBioView.findViewById<TextView>(R.id.tv_bio).text = dummyUser.bio
 
         // Add the block view to the root layout.
         rootLayout.addView(leadPhotoView)
