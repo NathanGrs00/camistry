96df5bbc6f501cf75cf417e50b754823a31323c7
NathanGRSDesktop <nathangeers00@gmail.com>
2025-05-31 21:52:35 +0200

    Refactor model classes to provide default values for properties

--- app/src/main/java/com/nathan/camistry/model/Connection.kt
+++ app/src/main/java/com/nathan/camistry/model/Connection.kt
@@ -3,7 +3,7 @@ package com.nathan.camistry.model
 import com.google.firebase.Timestamp
 
 data class Connection(
-    val userId1: String,
-    val userId2: String,
-    val timestamp: Timestamp
+    val userId1: String = "",
+    val userId2: String = "",
+    val timestamp: Timestamp = Timestamp.now(),
 )
--- app/src/main/java/com/nathan/camistry/model/Like.kt
+++ app/src/main/java/com/nathan/camistry/model/Like.kt
@@ -3,8 +3,8 @@ package com.nathan.camistry.model
 import com.google.firebase.Timestamp
 
 data class Like(
-    val fromUserId: String,
-    val toUserId: String,
-    val liked: Boolean,
-    val timestamp: Timestamp
+    val fromUserId: String = "",
+    val toUserId: String = "",
+    val liked: Boolean = false,
+    val timestamp: Timestamp = Timestamp.now()
 )
